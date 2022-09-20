bda79a108c03400e68d86589e013d6c9e7443170
josef.melkus <josef.melkus@bringauto.com>
2022-09-20 10:35:37 +0000

    BAAS-387 virtual-fleet rename and documentation update

--- scripts/local_build.sh
+++ scripts/local_build.sh
@@ -5,8 +5,8 @@ git clean -xdf
 cd ./internal/pkg/ba_proto
 go mod init ba_proto
 go mod tidy
-cd ../virtual_industrial_portal
-go mod init virtual_industrial_portal
+cd ../virtual_fleet
+go mod init virtual_fleet
 go mod tidy
 cd ../proto_helper
 go mod init proto_helper
@@ -15,11 +15,11 @@ cd ../../..
 go mod init internal/app/main
 go mod tidy
 go mod edit -replace ba_proto=./internal/pkg/ba_proto
-go mod edit -replace virtual_industrial_portal=./internal/pkg/virtual_industrial_portal
+go mod edit -replace virtual_fleet=./internal/pkg/virtual_fleet
 go mod edit -replace proto_helper=./internal/pkg/proto_helper
 go get ba_proto
-go get virtual_industrial_portal
+go get virtual_fleet
 go get proto_helper
 cd ./internal/app
-go build -o virtual-industrial-portal-app
-mv ./virtual-industrial-portal-app ../../virtual-industrial-portal-app
+go build -o virtual-fleet-app
+mv ./virtual-fleet-app ../../virtual-fleet-app
