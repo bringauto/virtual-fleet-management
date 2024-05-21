d1de3744010cc698c56f9788d32c7b7fff9167f7
Josef Melkus <josef.melkus@bringauto.com>
2024-05-21 14:22:07 +0200

    working version

--- cmd/virtual-fleet-management/main.go
+++ cmd/virtual-fleet-management/main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 	"virtual_fleet_management/pkg/http_client"
 	"virtual_fleet_management/pkg/scenario"
 	"virtual_fleet_management/pkg/simulation"
@@ -15,7 +17,7 @@ import (
 
 func main() {
 
-	hostIp := flag.String("host", "http://172.18.0.1:8081", "IPv4 address")
+	hostIp := flag.String("host", "http://127.0.0.1:8081", "IPv4 address")
 	apiKey := flag.String("api-key", "123456", "API key")
 	logPath := flag.String("log-path", "./", "Path for log file")
 	scenariosPath := flag.String("scenario-dir", "./scenarios/virtual_vehicle", "Path of scenarios folder")
@@ -23,6 +25,8 @@ func main() {
 
 	flag.Parse()
 	setUpLogger(*logPath)
+	setSignalHandler()
+
 	cars := scenario.GetCarIdList(*scenariosPath)
 
 	var allScenarios []scenario.Scenario
@@ -30,17 +34,45 @@ func main() {
 		allScenarios = append(allScenarios, scenario.GetScenario(car, *scenariosPath))
 	}
 
-	var simulations map[string]simulation.Simulation
+	client := http_client.CreateClient(*hostIp, *apiKey)
+
+	var simulations = make(map[string]*simulation.Simulation)
 	for _, currScenario := range allScenarios {
-		simulations[currScenario.CarId] = simulation.New(currScenario, *loop)
+		simulations[currScenario.CarId] = simulation.New(currScenario, *loop, client)
 	}
 
-	client := http_client.CreateClient(*hostIp, *apiKey)
-	client.AddStop(allScenarios[0].Routes[0].Stations[1])
-	//var url = *brokerIp + ":" + *brokerPort
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	//setSignalHandler()
-	//ip.Client.Start(server, "", "", *scenariosPath, *loop, 0)
+	startNewCars(client, simulations)
+	wg.Wait() // Wait for all goroutines to finish
+}
+
+func startNewCars(client *http_client.Client, simulations map[string]*simulation.Simulation) {
+	existingCars := make(map[int32]bool)
+
+	go func() { // Start a new goroutine
+		for {
+			cars := client.GetCars()
+			for _, car := range cars {
+				if _, exists := existingCars[*car.Id]; !exists {
+					// If the car is not in the existingCars map, it's a new car.
+					existingCars[*car.Id] = true
+
+					// Start the simulation for the new car.
+					if carSimulation, ok := simulations[car.Name]; ok {
+						carSimulation.SetCarId(car.Id)
+						go carSimulation.Start() // Start the simulation in a new goroutine
+					} else {
+						log.Printf("[INFO] New car connected: %v, this car doesn't have any available scenario", car.Name)
+					}
+				}
+			}
+
+			// Sleep for a while before the next iteration to avoid busy looping.
+			time.Sleep(10 * time.Second) // TODO timeout
+		}
+	}()
 }
 
 func setUpLogger(path string) {
@@ -60,7 +92,6 @@ func setSignalHandler() {
 	signal.Notify(ic, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-ic
-		//ip.Client.Disconnect()
 		fmt.Printf("[INFO] signal received, exiting\n")
 		os.Exit(0)
 	}()
--- pkg/simulation/Simulation.go
+++ pkg/simulation/Simulation.go
@@ -1,7 +1,6 @@
 package simulation
 
 import (
-	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -16,122 +15,113 @@ type CancelableTimer struct {
 }
 
 type Simulation struct {
-	loop bool
-	//scenario          scenario.Scenario //TODO needed?
-	currentMission    *scenario.MissionStruct
-	remainingMissions []scenario.MissionStruct
-	routeIds          map[string]int32
-	stopIds           map[string]int32
-	missionChanged    bool
-	startTimestamp    int64
-	missionTimer      CancelableTimer
-	client            http_client.Client
+	loop               bool
+	simulationScenario scenario.Scenario
+	currentMission     scenario.MissionStruct
+	remainingMissions  []scenario.MissionStruct
+	routeIds           map[string]int32
+	stopIds            map[string]int32
+	startTimestamp     int64
+	missionTimer       CancelableTimer
+	client             *http_client.Client
+	carId              *int32
 }
 
-func New(scenario2 scenario.Scenario, loop bool) Simulation {
-	simulation := Simulation{
-		//scenario:          scenario2,
-		missionTimer:      CancelableTimer{timer: nil, cancelTimer: make(chan struct{})},
-		loop:              loop,
-		remainingMissions: scenario2.Missions,
-	}
-	simulation.initDatabase(scenario2)
+func New(simulationScenario scenario.Scenario, loop bool, client *http_client.Client) *Simulation {
+	simulation := new(Simulation)
+	simulation.simulationScenario = simulationScenario
+	simulation.remainingMissions = simulationScenario.Missions
+	simulation.loop = loop
+	simulation.client = client
+	simulation.routeIds = make(map[string]int32)
+	simulation.stopIds = make(map[string]int32)
+	simulation.missionTimer = CancelableTimer{timer: nil, cancelTimer: make(chan struct{})}
+
+	simulation.initDatabase(simulationScenario)
 	return simulation
 }
 
-// TODO remove this
-func (simulation Simulation) markStopAsDone(stopToMark string) {
-	if len(simulation.currentMission.Stops) < 1 {
-		panic(fmt.Sprintf("[%v] Vehicle trying to mark wrong stop (%v) as done, received when no stop is on mission %v", simulation.topic, stopToMark, simulation.currentMission.Name))
-	}
-
-	if stopToMark == simulation.currentMission.Stops[0].Name {
-		simulation.currentMission.Stops = simulation.currentMission.Stops[1:]
-		simulation.missionChanged = true
-	} else {
-		return
-	}
-
-	simulation.updateMissionIfEmpty()
+func (simulation *Simulation) SetCarId(carId *int32) {
+	simulation.carId = carId
 }
 
-func (simulation Simulation) updateMissionIfEmpty() {
-	if len(simulation.currentMission.Stops) > 0 {
-		return
+func (simulation *Simulation) Start() {
+	if simulation.carId == nil {
+		log.Fatalf("[ERROR] Car ID is not set for: %v", simulation.simulationScenario.CarId)
 	}
-
-	if len(simulation.remainingMissions) > 0 {
-		log.Printf("[INFO] [%v] Car have finished mission %v, waiting for next scheduled mission", simulation.topic, simulation.currentMission.Name)
-		return
-	}
-
-	if simulation.loop {
-		log.Printf("[INFO] [%v] Car have finished all of its missions, starting simulation again", simulation.topic)
-		simulation.resetsimulation()
-		return
-	}
-
-	log.Printf("[INFO] [%v] All missions have been finished", simulation.topic)
+	log.Printf("[INFO] [%v] Starting simulation", simulation.simulationScenario.CarId)
+	simulation.resetSimulation()
 }
 
-func (simulation Simulation) markMissionAccepted() {
-	simulation.missionChanged = false
-}
-
-func (simulation Simulation) setNextMissionTimer() {
+func (simulation *Simulation) setNextMissionTimer() {
 	if len(simulation.remainingMissions) > 0 {
 		missionTimeOffset, err := strconv.ParseInt(simulation.remainingMissions[0].Timestamp, 10, 64)
 		if err != nil {
-			log.Printf("[INFO] [%v] Next mission (%v) timestamp has wrong format(%v), defaulting to 1 minute", simulation.topic, simulation.remainingMissions[0].Name, simulation.remainingMissions[0].Timestamp)
+			log.Printf("[INFO] [%v] Next mission (%v) timestamp has wrong format(%v), defaulting to 1 minute", simulation.simulationScenario.CarId, simulation.remainingMissions[0].Name, simulation.remainingMissions[0].Timestamp)
 			missionTimeOffset = 60
 		}
 		startNextMissionTimestamp := missionTimeOffset + simulation.startTimestamp
 		calculatedTimerTime := startNextMissionTimestamp - time.Now().Unix()
 		if calculatedTimerTime < 1 {
-			log.Printf("[WARNING] [%v] Calculated time to next mission (%v) seems wrong, defaulting to one minute", simulation.topic, calculatedTimerTime)
+			log.Printf("[WARNING] [%v] Calculated time to next mission (%v) seems wrong, defaulting to one minute", simulation.simulationScenario.CarId, calculatedTimerTime)
 			calculatedTimerTime = 60
 		}
-		log.Printf("[INFO] [%v] Next mission (%v) timestamp: %v, mission will start in %vs", simulation.topic, simulation.remainingMissions[0].Name, startNextMissionTimestamp, calculatedTimerTime)
+		log.Printf("[INFO] [%v] Next mission (%v) timestamp: %v, mission will start in %vs", simulation.simulationScenario.CarId, simulation.remainingMissions[0].Name, startNextMissionTimestamp, calculatedTimerTime)
 		simulation.startMissionTimer(int(calculatedTimerTime))
 	}
 }
 
-func (simulation Simulation) startMissionTimer(duration int) {
-	if simulation.missionTimer.Timer == nil {
-		simulation.missionTimer.Timer = time.NewTimer(time.Duration(duration) * time.Second)
+func (simulation *Simulation) startMissionTimer(duration int) {
+	if simulation.missionTimer.timer == nil {
+		simulation.missionTimer.timer = time.NewTimer(time.Duration(duration) * time.Second)
 
 	} else {
-		simulation.missionTimer.Timer.Reset(time.Duration(duration) * time.Second)
+		simulation.missionTimer.timer.Reset(time.Duration(duration) * time.Second)
 	}
 
 	go func() {
 		select {
-		case <-simulation.missionTimer.Timer.C:
+		case <-simulation.missionTimer.timer.C:
 			if len(simulation.currentMission.Stops) > 0 {
-				log.Printf("[WARNING] [%v] Mission (%v) timeout! Starting new mission (%v)\n", simulation.topic, simulation.currentMission.Name, simulation.remainingMissions[0].Name)
+				log.Printf("[WARNING] [%v] Mission (%v) timeout! Starting new mission (%v)\n", simulation.simulationScenario.CarId, simulation.currentMission.Name, simulation.remainingMissions[0].Name)
 			} else {
-				log.Printf("[INFO] [%v] Starting next scheduled mission (%v)\n", simulation.topic, simulation.remainingMissions[0].Name)
+				log.Printf("[INFO] [%v] Starting next scheduled mission (%v)\n", simulation.simulationScenario.CarId, simulation.remainingMissions[0].Name)
 			}
 			simulation.popNextMission()
-		case <-simulation.missionTimer.CancelTimer:
-			simulation.missionTimer.Timer = nil
+		case <-simulation.missionTimer.cancelTimer:
+			simulation.missionTimer.timer = nil
 		}
 	}()
 }
 
-func (simulation Simulation) resetsimulation() {
-	simulation.remainingMissions = simulation.simulationData.Missions
+func (simulation *Simulation) resetSimulation() {
+	simulation.remainingMissions = simulation.simulationScenario.Missions
 	simulation.startTimestamp = time.Now().Unix()
 	if len(simulation.remainingMissions) > 0 {
-		simulation.popNextMission()
+		simulation.setNextMissionTimer()
 	}
 }
 
-func (simulation Simulation) popNextMission() {
+func (simulation *Simulation) popNextMission() {
 	if len(simulation.remainingMissions) > 0 {
 		simulation.currentMission = simulation.remainingMissions[0]
 		simulation.remainingMissions = simulation.remainingMissions[1:]
+		simulation.postMissionOrders()
 		simulation.setNextMissionTimer()
-		simulation.missionChanged = true
+		//simulation.missionChanged = true // TODO delete old orders?
+	}
+
+	if simulation.loop {
+		log.Printf("[INFO] [%v] Car have finished all of its missions, starting simulation again", simulation.simulationScenario.CarId)
+		simulation.resetSimulation()
+	} else {
+		log.Printf("[INFO] [%v] All missions have been finished", simulation.simulationScenario.CarId) // TODO check order state first
+		// TODO finish simulation
+	}
+}
+
+func (simulation *Simulation) postMissionOrders() {
+	for _, stop := range simulation.currentMission.Stops {
+		simulation.client.AddOrder(*simulation.carId, simulation.stopIds[stop.Name], simulation.routeIds[simulation.currentMission.Route])
 	}
 }
