73abcb3d9aefaff669d8ae3b158d022aa99a0c47
Josef Melkus <josef.melkus@bringauto.com>
2024-05-28 11:08:52 +0200

    add config and scenario to install dir

--- build.sh
+++ build.sh
@@ -9,15 +9,9 @@ VERSION=$(sed -E -n 's/version=([^=]+)/\1/p' < version.txt)
 MACHINE=$(uname -m | sed -E 's/_/-/')
 
 INSTALL_DIR="./virtual-fleet-management_${VERSION}_${MACHINE}-linux"
-INSTALL_DIR_SCENARIOS="./virtual-fleet-management-scenarios_${VERSION}_${MACHINE}-linux"
 
 if [[ -d ${INSTALL_DIR} ]]; then
   echo "${INSTALL_DIR} already exist. Delete it please" >&2
-  exit 1
-fi
-
-if [[ -d ${INSTALL_DIR_SCENARIOS} ]]; then
-  echo "${INSTALL_DIR_SCENARIOS} already exist. Delete it please" >&2
   exit 1
 fi
 
@@ -25,15 +19,15 @@ go get virtual-fleet-management
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w'
 
 mkdir -p "${INSTALL_DIR}"
-mkdir -p "${INSTALL_DIR_SCENARIOS}"
+mkdir -p "${INSTALL_DIR}/scenarios/virtual_vehicle/"
+mkdir -p "${INSTALL_DIR}/config/"
 
-cp virtual-fleet-management       "${INSTALL_DIR}/"
-cp README.md                      "${INSTALL_DIR}/"
-#cp LICENSE                        "${INSTALL_DIR}/" // TODO: Uncomment this line after adding LICENSE file
-cp -r resources/scenarios/        "${INSTALL_DIR_SCENARIOS}/" # TODO: add all resources to installed package
+cp virtual-fleet-management                  "${INSTALL_DIR}/"
+cp README.md                                 "${INSTALL_DIR}/"
+#cp LICENSE                                  "${INSTALL_DIR}/" // TODO: Uncomment this line after adding LICENSE file
+cp -r resources/scenarios/virtual_vehicle/   "${INSTALL_DIR}/scenarios/virtual_vehicle/"
+cp resources/config/default.json             "${INSTALL_DIR}/config/config.json"
 
 zip -r "virtual-fleet-management_v${VERSION}_${MACHINE}-linux.zip" ${INSTALL_DIR}/
-zip -r "virtual-fleet-management-scenarios_v${VERSION}_${MACHINE}-linux.zip" ${INSTALL_DIR_SCENARIOS}/
 
 rm -fr "${INSTALL_DIR}"
-rm -fr "${INSTALL_DIR_SCENARIOS}"
