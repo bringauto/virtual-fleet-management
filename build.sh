baa4f7f037eecb719f9d3282a461749ba3e1835f
Josef Melkus <josef.melkus@bringauto.com>
2024-05-28 08:52:03 +0200

    build script, dockerfile

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+SCRIPT_DIR="$(dirname "$0")"
+cd ${SCRIPT_DIR}
+
 VERSION=$(sed -E -n 's/version=([^=]+)/\1/p' < version.txt)
 MACHINE=$(uname -m | sed -E 's/_/-/')
 
@@ -13,24 +16,24 @@ if [[ -d ${INSTALL_DIR} ]]; then
   exit 1
 fi
 
-if [[ -d ${INSTALL_DIR_TOOLS} ]]; then
-  echo "${INSTALL_DIR_TOOLS} already exist. Delete it please" >&2
+if [[ -d ${INSTALL_DIR_SCENARIOS} ]]; then
+  echo "${INSTALL_DIR_SCENARIOS} already exist. Delete it please" >&2
   exit 1
 fi
 
-go get virtual-fleet-manager
+go get virtual-fleet-management
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w'
 
 mkdir -p "${INSTALL_DIR}"
-mkdir -p "${INSTALL_DIR_TOOLS}"
+mkdir -p "${INSTALL_DIR_SCENARIOS}"
 
 cp virtual-fleet-management       "${INSTALL_DIR}/"
 cp README.md                      "${INSTALL_DIR}/"
-#cp LICENSE                        "${INSTALL_DIR}/"
+#cp LICENSE                        "${INSTALL_DIR}/" // TODO: Uncomment this line after adding LICENSE file
 cp -r scenarios                   "${INSTALL_DIR_SCENARIOS}/"
 
 zip -r "virtual-fleet-management_v${VERSION}_${MACHINE}-linux.zip" ${INSTALL_DIR}/
-zip -r "virtual-fleet-management-scenarios_v${VERSION}_${MACHINE}-linux.zip" ${INSTALL_DIR_TOOLS}/
+zip -r "virtual-fleet-management-scenarios_v${VERSION}_${MACHINE}-linux.zip" ${INSTALL_DIR_SCENARIOS}/
 
 rm -fr "${INSTALL_DIR}"
-rm -fr "${INSTALL_DIR_TOOLS}"
+rm -fr "${INSTALL_DIR_SCENARIOS}"
